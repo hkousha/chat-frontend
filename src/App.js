68ad458d9d8bdf55af629499d99e425a81ee3663
David Tang <dtang859@gmail.com>
2021-11-14 09:27:24 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "",
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.connection = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.connection.onopen = () => {
+      console.log("WebSocket connected");
+    };
+
+    this.connection.onclose = () => {
+      console.error("WebSocket disconnected");
+    };
+
+    this.connection.onerror = (error) => {
+      console.error("WebSocket failed to connect", error);
+    };
+
+    this.connection.onmessage = async (event) => {
+      const blob = event.data;
+      const message = await blob.text();
+
+      console.log("WebSocket received", message);
+
+      this.setState({
+        messages: [...this.state.messages, message],
+      });
+    };
+  }
+
+  componentWillUnmount() {
+    this.connection.close();
+  }
+
+  render() {
+    return (
+      <div>
+        <ul id="chat">
+          {this.state.messages.map((message, i) => {
+            return <li key={i}>{message}</li>;
+          })}
+        </ul>
+
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+
+            this.connection.send(this.state.message);
+            this.setState({ message: "" });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <textarea
+            rows="8"
+            cols="80"
+            id="message"
+            value={this.state.message}
+            onChange={(event) => {
+              this.setState({ message: event.target.value });
+            }}
+          ></textarea>
+          <br />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
